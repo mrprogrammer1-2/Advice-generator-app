d9db7d5b515592426af159f8bf3374eaf93f3ec1
mrprogrammer1-2 <sakatag681@gmail.com>
2024-11-19 15:42:58 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const api = 'https://api.adviceslip.com/advice'
+
+const initApp = async ()=> {
+    await fetch(api).then(response=> response.json())
+    .then(
+        data=> {
+            addToHtml(data)
+        }
+        )
+}
+
+initApp()
+
+
+function addToHtml(data) {
+    const quots = document.querySelector(".quots q");
+    const adviceNum = document.querySelector(".advice-num")
+
+    quots.innerHTML = data.slip.advice
+    adviceNum.innerHTML = data.slip.id
+}
+
+document.getElementById("generate").addEventListener("click", initApp)
+
